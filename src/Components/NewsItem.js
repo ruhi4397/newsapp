fb5f2cd86a68ee6f53560611ff1ef853d131f580
Ruhi <kashyapruhi4397@gmail.com>
2025-05-06 19:20:18 -0600

    y

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className=" badge rounded-pill bg-danger">{source} </span>
+        </div>
+
+        <img
+          src={
+            !imageUrl
+              ? "https://cdn.arstechnica.net/wp-content/uploads/2025/03/Gemini-app-1152x648.jpg"
+              : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text">
+            by{author} on {new Date(date).toGMTString()}
+          </p>
+
+          <a
+            href={newsUrl}
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-sm btn-dark"
+          >
+            Read More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
